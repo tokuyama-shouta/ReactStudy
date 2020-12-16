34df65e80a1f6fd1753deb71d99aceea43e56e6b
tokuyama shouta <toku1031ts@gmail.com>
2020-12-16 13:02:32 +0900

    クラスコンポーネント

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import {useState} from 'react'
+import React from 'react'
 import {List} from './List'
 
-function App() {
-  const [description,setDescription] = useState('クリック前の表示')
-
-  const changeDescription = () => {
-    setDescription('クリックあとの表示')
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {description: 'クリック前の表示です'}
   }
-  return (
-    <div>
-      {description}
-      <List title="取り扱い言語一覧"/>
-      <button onClick={changeDescription}>ボタン</button>
-    </div>
-  );
+  changeDescription() {
+    this.setState({
+      description: 'クリック後の表示です。'
+    })
+  }
+  render() {
+    const {description} = this.state;
+    return(
+      <>
+        {description}
+        <duv>クラスコンポーネントにしてみました。</duv>
+        <List title="取り扱い言語一覧"/>
+        <button onClick={() => this.changeDescription()}>ボタン</button>
+      </>
+
+    )
+  }
 }
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,11 +1,14 @@
+import React from 'react'
 
-export const List = (props) => {
-  const title = props.title;
-  return(
-    <div>
-      <h4>{title}</h4>
-      <div>Listです</div>
-    </div>
-  )
+export class List extends React.Component {
+    render(){
+      const {title} = this.props;
+      return(
+        <div>
+          <h4>{title}</h4>
+          <div>リストです。</div>
+        </div>
+      )
+    }
 }
 
