11d5a91ddfb772c9aa7aec40b0272f94d1bb466f
tokuyama shouta <toku1031ts@gmail.com>
2020-12-16 12:42:25 +0900

    コンポーネントのpropsとstate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import {List} from './List'
 
 function App() {
+  const [description,setDescription] = useState('クリック前の表示')
+
+  const changeDescription = () => {
+    setDescription('クリックあとの表示')
+  }
   return (
-    <div>ゼロから始めるReact</div>
+    <div>
+      {description}
+      <List title="取り扱い言語一覧"/>
+      <button onClick={changeDescription}>ボタン</button>
+    </div>
   );
 }
 
---
+++ src/List.js
@@ -0,0 +1,11 @@
+
+export const List = (props) => {
+  const title = props.title;
+  return(
+    <div>
+      <h4>{title}</h4>
+      <div>Listです</div>
+    </div>
+  )
+}
+
