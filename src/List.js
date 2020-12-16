7adf5db26a273394f31464fc96b793f2c5b2d609
tokuyama shouta <toku1031ts@gmail.com>
2020-12-16 13:42:29 +0900

    mapと切り替え

--- src/List.js
+++ src/List.js
@@ -1,10 +1,22 @@
+const LANGUAGES = [
+  'JavaScript',
+  'C++',
+  'Ruby',
+  'Java',
+  'PHP',
+  'GO'
+];
 
 export const List = (props) => {
   const title = props.title;
   return(
     <div>
       <h4>{title}</h4>
-      <div>Listです</div>
+      {
+        LANGUAGES.map((lang,index) => {
+          return <div key={index}>{lang}</div>
+        })
+      }
     </div>
   )
 }
